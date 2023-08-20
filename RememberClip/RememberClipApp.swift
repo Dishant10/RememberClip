a527df0232c1c4f6d536871b6923087670206ffc
Dishant Nagpal <dishantnagpal10@gmail.com>
2023-08-20 23:30:56 +0530

    Initial Commit

---
+++ RememberClip/RememberClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberClipApp.swift
+//  RememberClip
+//
+//  Created by Dishant Nagpal on 20/08/23.
+//
+
+import SwiftUI
+
+@main
+struct RememberClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
